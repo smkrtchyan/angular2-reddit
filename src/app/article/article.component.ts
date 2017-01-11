ac4ca593c3c9de110462a0e2ae1c5af35036f504
Sargis Mkrtchyan <s.mkrtchyan@testbirds.de>
2017-01-11 16:26:44 +0100

    add multiple rows and add new post func

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
-export class ArticleComponent implements OnInit {
 
-  constructor() { }
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+
+  constructor(){
+    this.article = new Article( 'Angulr 2', 'http://angular.io', 10);
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 
   ngOnInit() {
   }
